70a0797b9eb3cc8c796531913169935033fea7d5
Jeffry Luqman <jeffry.luqman@gmail.com>
2025-03-09 17:52:45 +0700

    init

---
+++ pemrograman_visual/src/main/java/com/unindra/pemrograman_visual/Pemrograman_visual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unindra.pemrograman_visual;
+
+/**
+ *
+ * @author jeffry
+ */
+public class Pemrograman_visual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
